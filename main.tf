c3ad6a328aed04d588d53fcb80920bdc1a49a947
gnavien <gnavien@gmail.com>
2024-09-05 09:20:22 +0530

    update

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo labauto ansible",
-
+      "sudo set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/gnavien/roboshop-ansible.git main.yml -e env=${var.env} -e role_name=${var.component}"
     ]
   }
