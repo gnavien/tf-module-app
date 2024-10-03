d7dc0cb05c6635a8234a468c8e9f77aea53b654d
gnavien <gnavien@gmail.com>
2024-10-03 16:08:43 +0530

    update

--- main.tf
+++ main.tf
@@ -144,43 +144,43 @@
 #  }
 #}
 #
-#resource "aws_security_group" "sg" {
-#  name        = "${var.component}-${var.env}-sg"
-#  description = "${var.component}-${var.env}-sg"
-#  vpc_id      = var.vpc_id
-#
-#  ingress {
-#    from_port = 22
-#    to_port   = 22
-#    protocol  = "tcp"
-#
-#    ingress {
-#      from_port   = 0
-#      to_port     = 0
-#      protocol    = "-1"
-#      cidr_blocks = ["0.0.0.0/0"]
-#    }
-#
-#    egress {
-#      from_port   = 0
-#      to_port     = 0
-#      protocol    = "-1"
-#      cidr_blocks = ["0.0.0.0/0"]
-#    }
-#
-#    tags = {
-#      Name = "${var.component}-${var.env}-sg"
-#    }
-#  }
-#}
-#
-#resource "aws_instance" "test" {
-#  ami           = data.aws_ami.ami.id
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = [aws_security_group.sg.id]
-#  subnet_id = var.subnet_id
-#
-#  tags = {
-#    Name = var.component
-#  }
-#}
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    ingress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "${var.component}-${var.env}-sg"
+    }
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = var.subnet_id
+
+  tags = {
+    Name = var.component
+  }
+}
